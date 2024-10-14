354d333783a85e2d4a8c8e882a91cb7c5f24b425
Hiroshi Muraoka <h.muraoka714@gmail.com>
2024-10-15 08:13:09 +0900

    feat: implement module class
    Signed-off-by: Hiroshi Muraoka <h.muraoka714@gmail.com>

--- __tests__/module.test.ts
+++ __tests__/module.test.ts
@@ -1,7 +1,7 @@
 import { Module } from '../src/module'
 import { JUnitReport as JUnitReportXML } from '../src/junit/xml'
 import { GotestsumReport } from '../src/junit/reporter/gotestsum'
-import { TestResult } from '../src/junit/type'
+import { JUnitReport, TestResult, TestCase } from '../src/junit/type'
 
 describe('module', () => {
   it('constructs a module', async () => {
@@ -11,9 +11,86 @@ describe('module', () => {
         { testsuites: {} },
       )
     )
-
-    const module = await Module.fromXML('path/to', 'junit.xml')
+    const module = await Module.fromXml('path/to', 'junit.xml')
     expect(module.directory).toBe('path/to')
     expect(fromXMLMock).toHaveBeenCalledWith('path/to/junit.xml')
+  })
+
+  it('makes a module table record', async () => {
+    const module = new Module('path/to', {
+      directory: 'path/to',
+      result: TestResult.Passed,
+      tests: 4,
+      passed: 3,
+      failed: 1,
+      skipped: 0,
+      time: 1.10,
+      version: '1.22.1',
+      failures: [],
+    } as JUnitReport)
+
+    expect(module.makeModuleTableRecord('owner', 'repo', 'sha')).toEqual(
+      '| [path/to](https://github.com/owner/repo/blob/sha/path/to) | 1.22.1 | ✅Passed | 3 | 1 | 0 | 1.1s |'
+    )
+  })
+
+  it('makes a failed test table record', async () => {
+    const module = new Module('path/to', {
+      directory: 'path/to',
+      result: TestResult.Failed,
+      tests: 4,
+      passed: 3,
+      failed: 1,
+      skipped: 0,
+      time: 1.10,
+      version: '1.22.1',
+      failures: [
+        new TestCase('path/to', 'foo/bar', 'baz_test.go', 1, 'Test1', 'error1\noccurred'),
+        new TestCase('path/to', 'foo/bar', 'baz_test.go', 2, 'Test2', 'error2\noccurred'),
+      ],
+    } as JUnitReport)
+
+    expect(module.makeFailedTestTableRecords('owner', 'repo', 'sha')).toEqual(`
+| [path/to/foo/bar/baz_test.go:1](https://github.com/owner/repo/blob/sha/path/to/foo/bar/baz_test.go#L1) | Test1 | error1 occurred |
+| [path/to/foo/bar/baz_test.go:2](https://github.com/owner/repo/blob/sha/path/to/foo/bar/baz_test.go#L2) | Test2 | error2 occurred |
+`.slice(1, -1))
+  })
+
+  it('makes a failed lint table record', async () => {
+    const module = new Module('path/to', {
+      directory: 'path/to',
+      result: TestResult.Failed,
+      tests: 4,
+      passed: 3,
+      failed: 1,
+      skipped: 0,
+      time: 1.10,
+      version: '1.22.1',
+      failures: [],
+    } as JUnitReport)
+
+    expect(module.makeFailedLintTableRecords()).toEqual('')
+  })
+
+  it('makes annotation messages', async () => {
+    const module = new Module('path/to', {
+      directory: 'path/to',
+      result: TestResult.Failed,
+      tests: 4,
+      passed: 3,
+      failed: 1,
+      skipped: 0,
+      time: 1.10,
+      version: '1.22.1',
+      failures: [
+        new TestCase('path/to', 'foo/bar', 'baz_test.go', 1, 'Test1', 'error1\noccurred'),
+        new TestCase('path/to', 'foo/bar', 'baz_test.go', 2, 'Test2', 'error2\noccurred'),
+      ],
+    } as JUnitReport)
+
+    expect(module.makeAnnotationMessages()).toEqual([
+      '::error file=path/to/foo/bar/baz_test.go,line=1::error1\noccurred',
+      '::error file=path/to/foo/bar/baz_test.go,line=2::error2\noccurred',
+    ])
   })
 })
--- src/module.ts
+++ src/module.ts
@@ -1,20 +1,63 @@
 import * as path from "path";
 import { GotestsumReport } from "./junit/reporter/gotestsum";
+import { JUnitReport, TestResult } from "./junit/type";
 
 export class Module {
   constructor(
     private readonly _directory: string,
-    private readonly _testReport: GotestsumReport
+    private readonly _testReport: JUnitReport
   ) {}
 
-  static async fromXML(
+  static async fromXml(
     directory: string,
     testPath: string,
   ): Promise<Module> {
     return new Module(directory, await GotestsumReport.fromXml(path.join(directory, testPath)))
   }
 
-  get directory(): string {
+  public get directory(): string {
     return this._directory
+  }
+
+  public makeModuleTableRecord(
+    owner: string,
+    repo: string,
+    sha: string,
+  ): string {
+    const link = `[${this._directory}](https://github.com/${owner}/${repo}/blob/${sha}/${this._directory})`
+    const version = this._testReport.version ?? '-'
+    const result = this._testReport.result === TestResult.Failed ? '❌Failed' : '✅Passed'
+    const passed = this._testReport.passed
+    const failed = this._testReport.failed
+    const skipped = this._testReport.skipped
+    const time = this._testReport.time?.toFixed(1).concat('s') ?? '-'
+    return `| ${link} | ${version} | ${result} | ${passed} | ${failed} | ${skipped} | ${time} |`
+  }
+
+  public makeFailedTestTableRecords(
+    owner: string,
+    repo: string,
+    sha: string,
+  ): string {
+    return this._testReport.failures.map(failure => {
+      const { fullPath, line, test, message } = failure
+      const fileTitle = `${fullPath}:${line}`
+      const fileLink = `https://github.com/${owner}/${repo}/blob/${sha}/${fullPath}#L${line}`
+      const fileColumn = `[${fileTitle}](${fileLink})`
+      const joinedMessage = message.replace(/\n/g, ' ')
+      return `| ${fileColumn} | ${test} | ${joinedMessage} |`
+    }).join('\n')
+  }
+
+  public makeFailedLintTableRecords(): string {
+    return ''
+  }
+
+  public makeAnnotationMessages(): string[] {
+    return this._testReport.failures.map(failure => {
+      const { subDir, file, line, message } = failure
+      const fullPath = path.join(this._directory, subDir, file)
+      return `::error file=${fullPath},line=${line}::${message}`
+    })
   }
 }
