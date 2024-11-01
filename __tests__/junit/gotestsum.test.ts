a7394e6adde3c8dd15b139fce47074deba6a06a3
Hiroshi Muraoka <h.muraoka714@gmail.com>
2024-11-01 22:58:44 +0900

    refactor: gotestsum test
    Signed-off-by: Hiroshi Muraoka <h.muraoka714@gmail.com>

--- __tests__/junit/gotestsum.test.ts
+++ __tests__/junit/gotestsum.test.ts
@@ -1,21 +1,44 @@
-import * as fs from 'fs'
-
-import { ReporterFactory } from '../../src/junit/factory'
-import { Result, Case } from '../../src/junit/reporter'
+import { JUnitReport } from '../../src/junit/xml'
+import { GotestsumReport } from '../../src/junit/gotestsum'
+import { Result } from '../../src/junit/reporter'
 
 describe('gotestsum', () => {
   const testCases = [
     {
       name: 'should parse the junit report with no testsuite',
-      input: `
-        <testsuites tests="0" failures="0" errors="0" time="0.000000">
-          <testsuite tests="0" failures="0" time="0.000000" name="." timestamp="2024-10-21T18:16:20+09:00">
-             <properties>
-               <property name="go.version" value="go1.23.2 linux/amd64"></property>
-             </properties>
-          </testsuite>
-        </testsuites>
-        `,
+      input: {
+        testsuites: {
+          $: {
+            tests: '0',
+            failures: '0',
+            errors: '0',
+            time: '0.000000'
+          },
+          testsuite: [
+            {
+              $: {
+                name: '.',
+                tests: '0',
+                failures: '0',
+                time: '0.000000',
+                timestamp: '2024-10-21T18:16:20+09:00'
+              },
+              properties: [
+                {
+                  property: [
+                    {
+                      $: {
+                        name: 'go.version',
+                        value: 'go1.23.2 linux/amd64'
+                      }
+                    }
+                  ]
+                }
+              ]
+            }
+          ]
+        }
+      } as JUnitReport,
       expected: {
         result: Result.Passed,
         tests: 0,
@@ -29,29 +52,72 @@ describe('gotestsum', () => {
     },
     {
       name: 'should parse the junit report with testsuite',
-      input: `
-      <?xml version="1.0" encoding="UTF-8"?>
-      <testsuites tests="4" failures="2" errors="0" time="0.001000">
-        <testsuite tests="2" failures="1" time="0.001000" name="foo" timestamp="2024-09-17T21:07:31+09:00">
-          <properties>
-            <property name="go.version" value="go1.22.1 linux/amd64"></property>
-          </properties>
-          <testcase classname="foo/bar" name="Test1" time="0.000000"></testcase>
-          <testcase classname="foo/bar" name="Test2" time="0.000000">
-            <failure message="Failed" type="">=== RUN   Test2&#xA;    baz_test.go:1: error;</failure>
-          </testcase>
-        </testsuite>
-        <testsuite tests="2" failures="1" time="0.001000" name="foo" timestamp="2024-09-17T21:07:31+09:00">
-          <properties>
-            <property name="go.version" value="go1.22.1 linux/amd64"></property>
-          </properties>
-          <testcase classname="foo/bar" name="Test3" time="0.000000"></testcase>
-          <testcase classname="foo/bar" name="Test4" time="0.000000">
-            <failure message="Failed" type="">=== RUN   Test4&#xA;--- FAIL: Test4 (0.00s)&#xA;</failure>
-          </testcase>
-        </testsuite>
-      </testsuites>
-      `,
+      input: {
+        testsuites: {
+          $: {
+            tests: '4',
+            failures: '2',
+            errors: '0',
+            time: '0.001000'
+          },
+          testsuite: [
+            {
+              $: {
+                name: 'foo',
+                tests: '2',
+                failures: '1',
+                time: '0.001000',
+                timestamp: '2024-09-17T21:07:31+09:00'
+              },
+              testcase: [
+                {
+                  $: {
+                    classname: 'foo/bar',
+                    name: 'Test1',
+                    time: '0.000500'
+                  }
+                },
+                {
+                  $: {
+                    classname: 'foo/bar',
+                    name: 'Test2',
+                    time: '0.000500'
+                  },
+                  failure: [
+                    {
+                      $: {
+                        message: 'Failed',
+                        type: ''
+                      },
+                      _: '=== RUN   Test2\n    baz_test.go:1: error;'
+                    },
+                    // This should be ignored
+                    {
+                      $: {
+                        message: 'Failed',
+                        type: ''
+                      },
+                      _: '=== RUN   Test2\n--- FAIL: Test2 (0.00s)&#xA;'
+                    }
+                  ]
+                }
+              ],
+              properties: [
+                {
+                  property: [
+                    {
+                      $: {
+                        name: 'go.version',
+                        value: 'go1.22.1 linux/amd64'
+                      }
+                    }
+                  ]
+                }
+              ]
+            }
+          ]
+        }
+      } as JUnitReport,
       expected: {
         result: Result.Failed,
         tests: 4,
@@ -74,10 +140,7 @@ describe('gotestsum', () => {
   ]
 
   it.each(testCases)('%s', async ({ input, expected }) => {
-    const readFileMock = jest
-      .spyOn(fs.promises, 'readFile')
-      .mockResolvedValue(input)
-    const report = await ReporterFactory.fromXml('test', 'path/to/junit.xml')
+    const report = new GotestsumReport(input)
     expect(report.result).toBe(expected.result)
     expect(report.tests).toBe(expected.tests)
     expect(report.passed).toBe(expected.passed)
@@ -85,9 +148,6 @@ describe('gotestsum', () => {
     expect(report.skipped).toBe(expected.skipped)
     expect(report.time).toBe(expected.time)
     expect(report.version).toBe(expected.version)
-    expect(report.failures).toEqual(expected.failures as Case[])
-    expect(readFileMock).toHaveBeenCalledWith('path/to/junit.xml', {
-      encoding: 'utf8'
-    })
+    expect(report.failures).toEqual(expected.failures)
   })
 })
