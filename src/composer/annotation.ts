f2667a2dec155294571882fb39806b5114e6588a
Hiroshi Muraoka <h.muraoka714@gmail.com>
2025-01-25 12:27:22 +0900

    fix: remove unintended change
    Signed-off-by: Hiroshi Muraoka <h.muraoka714@gmail.com>

--- src/composer/annotation.ts
+++ src/composer/annotation.ts
@@ -2,17 +2,10 @@ import { FailureReport } from '../report/type'
 
 export interface AnnotationComposer {
   toArray(failures: FailureReport[]): string[]
-  toArray(path: string, failures: FailureReport[]): string[]
 }
 
 export class AnnotationComposerImpl implements AnnotationComposer {
-  toArray(failuresOrPath: FailureReport[] | string, failures?: FailureReport[]): string[] {
-    if (typeof failuresOrPath === 'string') {
-      // パスが指定された場合
-      return (failures || []).map(failure => failure.annotation.record.body)
-    } else {
-      // パスが指定されていない場合
-      return failuresOrPath.map(failure => failure.annotation.record.body)
-    }
+  toArray(failures: FailureReport[]): string[] {
+    return failures.map(failure => failure.annotation.record.body)
   }
 }
